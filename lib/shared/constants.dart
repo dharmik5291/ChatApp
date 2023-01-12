613ecdf79817cc6c70f224b1298f489e2a4891b8
anghandharmik <anghandharmik11@gmail.com>
2023-01-12 10:22:26 +0530

    ChatApp

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:243715807403:web:80fbfd06d3f90875b97fee";
+  static String apiKey = "AIzaSyDvWmDYpEg-XrnMXN7M6RjheZ_5z-cBs6A";
+  static String messagingSenderId = "243715807403";
+  static String projectId = "chatapp-a7a2f";
+  final primaryColor = const Color(0xFFee7b64);
+}
